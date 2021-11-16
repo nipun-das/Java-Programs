6a08d34013d435d1ea26f20fa3638e3a70bd823a
C C Nipun Das <nipundas02@gmail.com>
2021-11-16 22:45:51 +0530

    Add files via upload

---
+++ Lab Programs/GreatestOf_3_Numbers.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class GreatestOf_3_Numbers {
+    public static void main(String args[]) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a + " is the greatest");
+            } else {
+                System.out.println(c + " is the greatest");
+            }
+        } else {
+            if (b > c) {
+                System.out.println(b + " is the greatest");
+            } else {
+                System.out.println(c + " is the greatest");
+            }
+        }
+    }
+}
