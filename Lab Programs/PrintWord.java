fd3dde398b1a0cbbd0b3386191ec3c13ccef04dc
C C Nipun Das <nipundas02@gmail.com>
2021-11-16 21:10:44 +0530

    Add files via upload

---
+++ Lab Programs/PrintWord.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PrintWord {
+
+    public static void main(String args[]) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String str = s.next();
+        System.out.println("Word entered is : " + str);
+
+    }
+
+}
