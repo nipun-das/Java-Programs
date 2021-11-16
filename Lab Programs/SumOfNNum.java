e385e1cb0dec6cddf0dee0e4c55a27da517dfe8d
C C Nipun Das <nipundas02@gmail.com>
2021-11-16 22:54:12 +0530

    Add files via upload

---
+++ Lab Programs/SumOfNNum.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SumOfNNum {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the limit: ");
+        int i = 0, sum = 0;
+        int n = s.nextInt();
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("The Sum up to " + n + " is " + sum);
+    }
+
+}
