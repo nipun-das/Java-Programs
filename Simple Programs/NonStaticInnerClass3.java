ce3a3039149afdf05b8a5648f27cee22330b6e01
C C Nipun Das <nipundas02@gmail.com>
2021-12-01 23:36:17 +0530

    Add files via upload

---
+++ Simple Programs/NonStaticInnerClass3.java
@@ -0,0 +1,25 @@
+package com.JavaPrograms2;
+
+class Outer3 {
+    int outer_x = 1;
+
+    void test() {
+        for (int i = 0; i < 10; i++) {
+            class Inner {
+                void display() {
+                    System.out.println("display: outer_x = " + outer_x);
+                    outer_x+=1;
+                }
+            }
+        Inner inner = new Inner();
+        inner.display();
+        }
+    }
+}
+
+public class NonStaticInnerClass3 {
+    public static void main(String args[]) {
+        Outer3 outer = new Outer3();
+        outer.test();
+    }
+}
