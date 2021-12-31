78aa75f557368a1c012c7513705b6169ddce17e1
C C Nipun Das <nipundas02@gmail.com>
2021-12-31 19:53:13 +0530

    Add files via upload

---
+++ Basic Programs/SecondSmallestAndSmallest.java
@@ -0,0 +1,38 @@
+package com.JavaPrograms2;
+
+//Implement a JAVA program to display the smallest and second-smallest element of an array.
+
+import java.util.Scanner;
+
+public class SecondSmallestAndSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, i, smallest, secondSmallest;
+        System.out.println("Enter the size of the array : ");
+        n = sc.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter the elements of the array : ");
+        for (i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        smallest = a[0];
+        secondSmallest = a[1];
+        for (i = 0; i < n; i++) {
+            if (a[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = a[i];
+            } else if ((a[i] > smallest) && (a[i] < secondSmallest)) {
+                secondSmallest = a[i];
+            }
+        }
+        System.out.println();
+        System.out.print("The entered array is : ");
+        for (i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Smallest in the array is : " + smallest);
+        System.out.println("Second smallest in the array is : " + secondSmallest);
+    }
+}
+
